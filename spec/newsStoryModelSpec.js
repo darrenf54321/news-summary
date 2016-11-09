1d66764c7dcb45d90bd01055485805b061d753fb
Darrenf <darrenf54321@gmail.com>
2016-11-09 12:26:48 +0000

    1. Failing spec test for news summary 2. Commenced work on SummaryArticle method

--- spec/newsStoryModelSpec.js
+++ spec/newsStoryModelSpec.js
@@ -8,7 +8,15 @@ describe("News Story Model", function() {
   });
 
   it("returns a list of all today's headlines", function() {
-    newsStoryModel.addArticle("news");
-    expect(newsStoryModel.getAll()).toEqual(["news"])
+    newsStoryModel.addArticle("news article 1");
+    newsStoryModel.addArticle("news article 2");
+    newsStoryModel.addArticle("news article 3");
+    expect(newsStoryModel.getAll()).toEqual(["news article 1", "news article 2", "news article 3"])
+  });
+
+  it("returns a summary of the news articles", function () {
+    newsStoryModel.addArticle("Heading 1 news article 1");
+    newsStoryModel.getAll();
+    expect(newsStoryModel.summaryArticle()).toEqual("Heading 1");
   });
 });
